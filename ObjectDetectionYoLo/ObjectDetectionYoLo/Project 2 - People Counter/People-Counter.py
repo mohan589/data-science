9cc11460ad4151d5202aa312b0e7ccf10176d04e
Mohan Pichikala <mpichikala@intralinks.com>
2024-09-16 20:36:44 +0530

    pytorch updates

---
+++ ObjectDetectionYoLo/ObjectDetectionYoLo/Project 2 - People Counter/People-Counter.py
@@ -0,0 +1,84 @@
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+
+# cap = cv2.VideoCapture(0) #Web Cam
+# cap.set(3, 1280)
+# cap.set(4, 640)
+
+cap = cv2.VideoCapture('../Videos/people.mp4')
+cap.set(3, 1280)
+cap.set(4, 720)
+
+model = YOLO('Yolo-Weights/yolov8n.pt') # Nano
+# model = YOLO('Yolo-Weights/yolov8l.pt') # Large
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+# Tracking
+tracker = Sort(max_age=20, min_hits=2, iou_threshold=0.3)
+limits = [410, 297, 673, 297]
+totalCount = []
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    imgGraphics = cv2.imread("img.png", cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
+
+    detections = np.empty((0, 5))
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # x1, y1, w, h = box.xywh[0]
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.3:
+              w, h = x2 - x1, y2 - y1
+              bbox = int(x1), int(y1), int(w), int(h)
+              cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+              currentArray = np.array([x1, y1, x2, y2, conf])
+              detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    for result in resultsTracker:
+      x1, y1, x2, y2, id = result
+      x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+      print(result)
+      w, h = x2 - x1, y2 - y1
+      cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 0))
+      cvzone.putTextRect(img, f"{int(id)}",
+                         (max(0, x1), max(35, y1)),
+                         scale=2,
+                         thickness=3,
+                         offset=10)
+
+      cx, cy = x1 + w //2, y1+h//2
+      cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+      if limits[0]< cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+        if totalCount.count(id) == 0:
+          totalCount.append(id)
+
+    cv2.putText(img, str(len(totalCount)), (200, 90), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
+    cv2.putText(img, str(len(totalCount)), (450, 90), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
