45020588835119d3c4d4d072ad0b95a647cb2702
Mohan <mohan589@users.noreply.github.com>
2024-09-28 19:20:08 +0530

    Mobile support

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,7 +2,9 @@ import cv2
 import time
 import numpy as np
 
-cam = cv2.VideoCapture(0)
+# cam = cv2.VideoCapture('rtsp://192.168.0.39:8080/h264_ulaw.sdp') # To access from Mobile, we need ipwebcam and server should be up
+
+cam = cv2.VideoCapture('rtsp://192.168.0.39:8080/h264_ulaw.sdp')
 
 wCam, hCam = 640, 480
 cam.set(3, wCam)
@@ -17,4 +19,7 @@ while True:
   pTime = cTime
   cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
   cv2.imshow("Img", img)
-  cv2.waitKey(1)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+cam.release()
+cv2.destroyAllWindows()
